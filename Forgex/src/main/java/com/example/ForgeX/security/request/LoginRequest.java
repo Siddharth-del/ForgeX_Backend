6d3a39f615a7b8731e952a16319720beb655f168
Siddharth <gautamsiddharth131004@gmail.com>
2026-09-23 19:31:57 +0530

    Add Lombok annotations for LoginRequest class

--- Forgex/src/main/java/com/example/ForgeX/security/request/LoginRequest.java
+++ Forgex/src/main/java/com/example/ForgeX/security/request/LoginRequest.java
@@ -2,9 +2,13 @@ package com.example.ForgeX.security.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor 
+@NoArgsConstructor 
 public class LoginRequest {
 
     @NotBlank
